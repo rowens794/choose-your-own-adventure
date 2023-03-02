debc8900c3eb54d3ad6806e804c2bd5c80e0f1d1
rowens794 <rowens794@gmail.com>
2023-03-01 20:03:03 -0500

    forced inactive buttons on load

--- pages/game.tsx
+++ pages/game.tsx
@@ -10,10 +10,12 @@ export default function Home() {
     userActions: [{ action: "Start Your Nightmare", result: "Game Continues" }],
     gameOver: false,
   });
+  const [loading, setLoading] = useState(false);
 
   //make a request to the getStep endpoint
   const makeRequest = async (choice: string) => {
     //create a post request with the choice in the body
+    setLoading(true);
     const response = await fetch("/api/getStep", {
       method: "POST",
       headers: {
@@ -26,6 +28,7 @@ export default function Home() {
     });
     const { data } = await response.json();
     setGameBoard(data);
+    setLoading(false);
   };
 
   return (
@@ -40,23 +43,34 @@ export default function Home() {
         className="bg-center bg-cover min-h-screen w-full relative z-20"
         style={{ minHeight: "-webkit-fill-available" }}
       >
-        <div className="grid grid-rows-3 h-screen">
-          <div className="row-span-2 p-8">
+        <div className="min-h-screen">
+          <div className="p-8">
             <p className="text-white mt-8 text-lg leading-8 font-light">
               {gameBoard?.nextPassage}
             </p>
           </div>
           <div className="text-center w-full h-12 flex flex-col gap-4 p-8">
             {gameBoard?.userActions.map(({ action }) => (
-              <button
-                key={action}
-                className="bg-fuchsia-900 text-white text-xl p-4"
-                onClick={() => {
-                  makeRequest(action);
-                }}
-              >
-                {action}
-              </button>
+              <>
+                {loading ? (
+                  <button
+                    key={action}
+                    className="bg-gray-700 text-white text-xl p-4"
+                  >
+                    {action}
+                  </button>
+                ) : (
+                  <button
+                    key={action}
+                    className="bg-fuchsia-900 text-white text-xl p-4"
+                    onClick={() => {
+                      makeRequest(action);
+                    }}
+                  >
+                    {action}
+                  </button>
+                )}
+              </>
             ))}
           </div>
         </div>
