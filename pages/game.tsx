408a30a87608a4219815384cfe3395b81a1b6265
rowens794 <rowens794@gmail.com>
2023-03-04 22:18:14 -0500

    changed vh to perc

--- pages/game.tsx
+++ pages/game.tsx
@@ -48,7 +48,7 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <main
-        className="bg-center bg-cover h-[calc(100vh-200px)] w-full relative z-20"
+        className="bg-center bg-cover h-[calc(100%-200px)] w-full relative z-20"
         style={{ minHeight: "-webkit-fill-available" }}
       >
         <div className="h-screen flex flex-col">
@@ -57,7 +57,7 @@ export default function Home() {
           <div className="flex justify-between flex-col flex-grow h-full overflow-hidden">
             {/*Main Content Window*/}
             <div
-              className={`max-h-full z-20 relative overflow-scroll h-[calc(100vh-200px)] flex justify-between flex-col transform transition-transform duration-300 bg-black  ${
+              className={`max-h-full z-20 relative overflow-scroll h-[calc(100%-200px)] flex justify-between flex-col transform transition-transform duration-300 bg-black  ${
                 loading && " translate-x-full"
               }`}
             >
